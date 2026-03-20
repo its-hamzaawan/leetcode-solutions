a88a3a16fd8645582fbfa2bc2839ff4e6baffa00
Hamza Awan <itshamzaawan1@gmail.com>
2026-03-20 11:42:38 +0500

    Time: 6 ms (40.20%) | Memory: 27.3 MB (67.05%) - LeetSync

---
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<int,int>mp;
+    int preIndex = 0;
+    TreeNode* build(vector<int>& preorder, vector<int>& inorder,int inStart, int inEnd){
+     if(inStart > inEnd) return NULL;
+     int rootVal = preorder[preIndex++];
+     TreeNode* root = new TreeNode(rootVal);
+      int index = mp[rootVal];
+      root->left = build(preorder,inorder,inStart,index-1);
+      root->right = build(preorder,inorder,index+1,inEnd);
+
+      return root;
+    }
+
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        int n = inorder.size();
+        for(int i =0;i<n;i++){
+            mp[inorder[i]] = i;
+        }
+        return build(preorder,inorder,0,inorder.size()-1);
+    }
+};
